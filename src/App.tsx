4464d0305377b9aa0bbbc1115fe18a4731467599
phamducai <giacat2910@gmail.com>
2023-03-01 19:39:53 +0700

    'huhu

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "pages/login/Home";
+import Login from "pages/login/Login";
+import Sign from "pages/login/Sign";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home />}>
+          <Route index path="/" element={<Login />} />
+          <Route path="/sign" element={<Sign />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  corePlugins: {
+    preflight: false,
+  },
+};
